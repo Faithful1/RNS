0988e009966897e64ea974b7fec452e3bb364238
fanere <fanere@deloitte.de>
2024-10-28 22:29:29 +0100

    feat(lb): Add condition to control creation of load balancer

--- config/prod.tfvars
+++ config/prod.tfvars
@@ -14,10 +14,20 @@ loadbalancer_name = "https-lb-static-backend"
 default_redirect_url = "https://www.google.com"
 
 frontend_config_map = {
-    "genesis" = {
+    genesis = {
         service_name               = "genesis"
         environment                = "prod"
         domain_name               = "genesis.service.rns.ai." # Example existing frontend domain
+        dnszone                   = "service-rns-ai"  # MAKE SURE THIS DNSZONE EXISTS IN THE PROJECT
+        dnszone_project_id        = "dgcp-sandbox-faithful-anere" # THIS IS THE PROJECT ID OF THE DNSZONE
+        enable_cdn                   = true
+        enable_versioning = true
+        bucket_push_account_email = "fanere@deloitte.de"
+    }
+    exodus = {
+        service_name               = "exodus"
+        environment                = "prod"
+        domain_name               = "exodus.service.rns.ai." # Example existing frontend domain
         dnszone                   = "service-rns-ai"  # MAKE SURE THIS DNSZONE EXISTS IN THE PROJECT
         dnszone_project_id        = "dgcp-sandbox-faithful-anere" # THIS IS THE PROJECT ID OF THE DNSZONE
         enable_cdn                   = true
--- gcp-loadbalancer/gcp__lb.tf
+++ gcp-loadbalancer/gcp__lb.tf
@@ -1,8 +1,5 @@
-# NOTE: this is a level 7 ssl loadbalancer with backend bucket
-#       so you should set them according toconfig/<envname>.tfvars file
-
 module "backend" {
-  for_each = var.frontend_config_map
+  for_each = var.create_loadbalancer ? var.frontend_config_map : {}
 
   source = "./lb-backend_bucket"
 
@@ -16,7 +13,7 @@ module "backend" {
 }
 
 resource "google_compute_managed_ssl_certificate" "main" {
-  for_each = var.frontend_config_map
+  for_each = var.create_loadbalancer ? var.frontend_config_map : {}
 
   project = var.project_id
   name    = "cert-${var.loadbalancer_name}-${each.value.service_name}-${each.value.environment}"
@@ -29,18 +26,20 @@ resource "google_compute_managed_ssl_certificate" "main" {
 }
 
 resource "google_compute_target_https_proxy" "main" {
-  project = var.project_id
+  count = var.create_loadbalancer ? 1 : 0
 
+  project          = var.project_id
   name             = var.loadbalancer_name
-  ssl_policy       = google_compute_ssl_policy.main.self_link
-  url_map          = google_compute_url_map.main.self_link
+  ssl_policy       = google_compute_ssl_policy.main[0].self_link
+  url_map          = google_compute_url_map.main[0].self_link
   ssl_certificates = [for cert in google_compute_managed_ssl_certificate.main : cert.self_link]
 }
 
 resource "google_compute_url_map" "main" {
-  project = var.project_id
+  count = var.create_loadbalancer ? 1 : 0
 
-  name = "${var.loadbalancer_name}-url-map"
+  project = var.project_id
+  name    = "${var.loadbalancer_name}-url-map"
 
   default_url_redirect {
     host_redirect          = var.default_redirect_url
@@ -49,7 +48,7 @@ resource "google_compute_url_map" "main" {
   }
 
   dynamic "host_rule" {
-    for_each = [for idx, config in var.frontend_config_map : config]
+    for_each = var.create_loadbalancer ? [for idx, config in var.frontend_config_map : config] : []
     content {
       hosts        = [host_rule.value["domain_name"]]
       path_matcher = "${host_rule.value["service_name"]}-${host_rule.value["environment"]}-allpaths"
@@ -57,7 +56,7 @@ resource "google_compute_url_map" "main" {
   }
 
   dynamic "path_matcher" {
-    for_each = [for idx, config in var.frontend_config_map : config]
+    for_each = var.create_loadbalancer ? [for idx, config in var.frontend_config_map : config] : []
     content {
       name            = "${path_matcher.value["service_name"]}-${path_matcher.value["environment"]}-allpaths"
       default_service = module.backend[path_matcher.value["service_name"]].backend_name
@@ -74,6 +73,7 @@ resource "google_compute_url_map" "main" {
 }
 
 resource "google_compute_ssl_policy" "main" {
+  count           = var.create_loadbalancer ? 1 : 0
   project         = var.project_id
   name            = "main-ssl-policy"
   profile         = "RESTRICTED"
@@ -81,26 +81,30 @@ resource "google_compute_ssl_policy" "main" {
 }
 
 resource "google_compute_global_forwarding_rule" "main" {
+  count = var.create_loadbalancer ? 1 : 0
+
   project    = var.project_id
-  ip_address = google_compute_global_address.main.address
+  ip_address = google_compute_global_address.main[0].address
   name       = "frontend-lb-forwarding-rule"
-  target     = google_compute_target_https_proxy.main.self_link
+  target     = google_compute_target_https_proxy.main[0].self_link
   port_range = 443
 }
 
-## DNS Entries
 resource "google_dns_record_set" "a_record" {
-  for_each = { for idx, config in var.frontend_config_map : idx => config if contains(keys(config), "dnszone") }
-  project  = try(each.value.dnszone_project_id, var.project_id)
+  for_each = var.create_loadbalancer ? { for idx, config in var.frontend_config_map : idx => config if contains(keys(config), "dnszone") } : {}
+
+  project = try(each.value.dnszone_project_id, var.project_id)
 
   name         = each.value.domain_name
   type         = "A"
   ttl          = 300
   managed_zone = each.value.dnszone
-  rrdatas      = [google_compute_global_address.main.address]
+  rrdatas      = [google_compute_global_address.main[0].address]
 }
 
 resource "google_compute_global_address" "main" {
+  count = var.create_loadbalancer ? 1 : 0
+
   project    = var.project_id
   name       = "frontend-lb-ip"
   ip_version = "IPV4"
